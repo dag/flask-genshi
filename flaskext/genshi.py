9e15480f87172e0b73c3414cfed7c39d439b1ddd
Dag Odenhall <dag.odenhall@gmail.com>
2010-05-28 14:19:40 +0200

    Stub generated with make-flaskext

---
+++ flaskext/genshi.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    flaskext.genshi
+    ~~~~~~~~~~~~~~~
+
+    Description of the module goes here...
+
+    :copyright: (c) 2010 by Dag Odenhall <dag.odenhall@gmail.com>.
+    :license: BSD, see LICENSE for more details.
+"""
---
+++ setup.py
@@ -0,0 +1,45 @@
+"""
+Flask-Genshi
+------------
+
+Description goes here...
+
+Links
+`````
+
+* `documentation <http://packages.python.org/Flask-Genshi>`_
+* `development version
+  <http://bitbucket.org/USERNAME/REPOSITORY/get/tip.gz#egg=Flask-Genshi-dev`_
+
+
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-Genshi',
+    version='0.1',
+    url='<enter URL here>',
+    license='BSD',
+    author='Dag Odenhall <dag.odenhall@gmail.com>',
+    author_email='your-email-here@example.com',
+    description='<enter short description here>',
+    long_description=__doc__,
+    packages=['flaskext'],
+    namespace_packages=['flaskext'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
