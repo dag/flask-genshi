f3733c1f73e8529f36a0cdb3d920963bd8fe6b03
Dag Odenhall <dag.odenhall@gmail.com>
2010-10-23 11:35:46 +0200

    Move long description to readme

--- README.rst
+++ README.rst
@@ -1,8 +1,31 @@
 Flask-Genshi
-============
+------------
 
-An extension to Flask for easy Genshi templating.
+`Flask-Genshi`_ is an extension to `Flask`_ that allows you to easily
+use `Genshi`_ for templating. Easily switch between HTML5 and XHTML
+and have the mimetype set automatically for you.
 
-Documentation: http://packages.python.org/Flask-Genshi
+::
 
-Thanks Armin Ronacher for the logo (and for Flask of course!)
+    from flask import Flask
+    from flaskext.genshi import Genshi, render_response
+
+    app = Flask(__name__)
+    genshi = Genshi(app)
+
+    @app.route('/')
+    def index():
+        title = 'Genshi + Flask, a match made in heaven!'
+        return render_response('index.html', dict(title=title))
+
+You can install the `development version`_ from `GitHub`_
+with ``easy_install Flask-Genshi==dev`` but you probably want the
+latest stable release::
+
+    sudo easy_install Flask-Genshi
+
+.. _Flask-Genshi: http://packages.python.org/Flask-Genshi/
+.. _Flask: http://flask.pocoo.org/
+.. _Genshi: http://genshi.edgewall.org/
+.. _development version: http://github.com/dag/flask-genshi/zipball/master#egg=Flask-Genshi-dev
+.. _GitHub: http://github.com/dag/flask-genshi
--- setup.py
+++ setup.py
@@ -1,38 +1,9 @@
 #!/usr/bin/env python
-"""
-Flask-Genshi
-------------
-
-`Flask-Genshi`_ is an extension to `Flask`_ that allows you to easily
-use `Genshi`_ for templating. Easily switch between HTML5 and XHTML
-and have the mimetype set automatically for you.
-
-::
-
-    from flask import Flask
-    from flaskext.genshi import Genshi, render_response
-
-    app = Flask(__name__)
-    genshi = Genshi(app)
-
-    @app.route('/')
-    def index():
-        title = 'Genshi + Flask, a match made in heaven!'
-        return render_response('index.html', dict(title=title))
-
-You can install the `development version`_ from `GitHub`_
-with ``easy_install Flask-Genshi==dev`` but you probably want the
-latest stable release::
-
-    sudo easy_install Flask-Genshi
-
-.. _Flask-Genshi: http://packages.python.org/Flask-Genshi/
-.. _Flask: http://flask.pocoo.org/
-.. _Genshi: http://genshi.edgewall.org/
-.. _development version: http://github.com/dag/flask-genshi/zipball/master#egg=Flask-Genshi-dev
-.. _GitHub: http://github.com/dag/flask-genshi
-"""
 from setuptools import setup
+
+
+with open('README.rst') as file:
+    readme = file.read()
 
 
 setup(
@@ -43,7 +14,7 @@ setup(
     author='Dag Odenhall',
     author_email='dag.odenhall@gmail.com',
     description='An extension to Flask for easy Genshi templating.',
-    long_description=__doc__,
+    long_description=readme,
     packages=['flaskext'],
     namespace_packages=['flaskext'],
     zip_safe=False,
