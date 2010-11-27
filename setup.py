a3cb43b2a41679e452f2aade35193ee08503ddb9
Dag Odenhall <dag.odenhall@gmail.com>
2010-11-27 16:09:45 +0100

    Release: 0.5.1; fix failing installs

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.rst') as file:
 
 setup(
     name='Flask-Genshi',
-    version='0.5',
+    version='0.5.1',
     url='http://packages.python.org/Flask-Genshi',
     license='BSD',
     author='Dag Odenhall',
